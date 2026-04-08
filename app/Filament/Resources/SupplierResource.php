139ae0c927fc18c546071dee3339be03ef7704c0
ismimaharanirachman-code <ismimaharanirachman@gmail.com>
2026-04-08 12:29:28 +0700

    edit

--- app/Filament/Resources/SupplierResource.php
+++ app/Filament/Resources/SupplierResource.php
@@ -75,7 +75,7 @@ class SupplierResource extends Resource
         return [
             'index' => Pages\ListSuppliers::route('/'),
             'create' => Pages\CreateSupplier::route('/create'),
-            'edit' => Pages\EditSupplier::route('/{record}/edit'),
+          
         ];
     }
 }
